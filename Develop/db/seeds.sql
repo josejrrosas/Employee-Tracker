9ff3b126a0d8f6a95bd41e9c713c6cd4b3876d52
Jose Rosas <josejrrosas@yahoo.com>
2021-07-26 21:04:01 -0500

    added queries to seeds, created prompt with answers in Index, added server , made connection

---
+++ Develop/db/seeds.sql
@@ -0,0 +1,26 @@
+INSERT INTO departments (dept_name)
+VALUES ("Sales"),
+        ("Engineering"), 
+        ("Finance"), 
+        ("Legal");
+
+INSERT INTO roles (title, salary, dept_id, )
+VALUES ("Sales Lead", 100000, 0001)
+        ("Salesperson", 80000, 0001)
+        ("Lead Engineer", 150000, 0002)
+        ("Software Engineer", 120000, 0002)
+        ("Account Manager", 160000, 0003)
+        ("Accountant", 125000, 0003)    
+        ("Legal Team Lead", 250000, 0004)
+        ("Lawyer", 190000, 0004);
+       
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Juniper", "Lee", 1000, null)
+       ("Kyle", "Sheppard", 1001, "Juniper Lee")
+       ("Rebecca", "Jones", 2000, null)
+       ("Don", "Mattingly", 2002, "Rebecca Jones")
+       ("Michael", "Young", 3000, null)
+       ("Lauren", "Richie", 3003, "Michael Young")
+       ("Robert", "Johnson", 4000, null)
+       ("Leroy", "Jenkins", 4004, "Robert Johnson");
+
---
+++ config/connection.js
@@ -0,0 +1,31 @@
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: 'process.env.DB_HOST',
+    dialect: 'mysql',
+    port: 3306
+  }
+);
+
+sequelize.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    console.log(`
+    ╔═══╗─────╔╗──────────────╔═╗╔═╗
+    ║╔══╝─────║║──────────────║║╚╝║║
+    ║╚══╦╗╔╦══╣║╔══╦╗─╔╦══╦══╗║╔╗╔╗╠══╦═╗╔══╦══╦══╦═╗
+    ║╔══╣╚╝║╔╗║║║╔╗║║─║║║═╣║═╣║║║║║║╔╗║╔╗╣╔╗║╔╗║║═╣╔╝
+    ║╚══╣║║║╚╝║╚╣╚╝║╚═╝║║═╣║═╣║║║║║║╔╗║║║║╔╗║╚╝║║═╣║
+    ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
+    ───────║║──────╔═╝║─────────────────────╔═╝║
+    ───────╚╝──────╚══╝─────────────────────╚══╝`)
+    // runs the app
+    firstPrompt();
+});
+
+module.exports = sequelize;
